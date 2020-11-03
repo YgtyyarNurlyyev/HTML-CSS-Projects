918332d3edc66e66581f2a00f5bdd2ab8114e23b
unknown <ygtyyarnurlyyew@gmail.com>
2020-11-04 02:07:42 +0300

    Piano

---
+++ Piano/script/main.js
@@ -0,0 +1,59 @@
+function playC(){
+    var c = document.getElementById('c');
+    c.play();
+}
+
+function playDb(){
+    var db = document.getElementById('db');
+    db.play();
+}
+
+function playD(){
+    var d = document.getElementById('d');
+    d.play();
+}
+
+function playEb(){
+    var eb = document.getElementById('eb');
+    eb.play();
+}
+
+function playE(){
+    var e = document.getElementById('e');
+    e.play();
+}
+
+function playF(){
+    var f = document.getElementById('f');
+    f.play();
+}
+
+function playGb(){
+    var gb = document.getElementById('gb');
+    gb.play();
+}
+
+function playG(){
+    var g = document.getElementById('g');
+    g.play();
+}
+
+function playAb(){
+    var ab = document.getElementById('ab');
+    ab.play();
+}
+
+function playA(){
+    var a = document.getElementById('a');
+    a.play();
+}
+
+function playBb(){
+    var bb = document.getElementById('bb');
+    bb.play();
+}
+
+function playB(){
+    var b = document.getElementById('b');
+    b.play();
+}
